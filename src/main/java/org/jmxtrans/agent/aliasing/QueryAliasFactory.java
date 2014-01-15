2b20f62f95631ff4fb09804daef79a0454850103
Stefan Sperber <stefan.sperber@mail.rakuten.com>
2014-01-15 10:44:36 +0900

    Added QueryAliasFactory#postConstruct method for passing in configuration. Added empty implementation to DefaultQueryAliasFactory.

--- src/main/java/org/jmxtrans/agent/aliasing/QueryAliasFactory.java
+++ src/main/java/org/jmxtrans/agent/aliasing/QueryAliasFactory.java
@@ -4,8 +4,12 @@ import javax.management.ObjectName;
 
 import org.jmxtrans.agent.Query;
 
+import java.util.Map;
+
 public interface QueryAliasFactory {
 
+	void postConstruct(Map<String, String> settings);
+
 	String valueOf(Query query, ObjectName objectName, String key);
-	
+
 }
